3d01021138b68d00a36c7d731f96fa33b426ad2d
samarth <samarthchoprapws@gmail.com>
2023-11-29 18:46:44 -0500

    made occupying another organism's position illegal

--- src/utils/move.py
+++ src/utils/move.py
@@ -14,7 +14,8 @@ def move_right(world: np.ndarray, world_state: np.ndarray, organism):
     :param world_state: internal world state
     :return: new position (y, x)
     """
-    if organism.position['x'] != world.shape[1] - 1:
+    if organism.position['x'] != world.shape[1] - 1 and \
+            world_state[organism.position['y']][organism.position['x'] + 1] == 0:
         world[organism.position['y'], organism.position['x']] = WORLD_BACKGROUND_COLOR
         world_state[organism.position['y'], organism.position['x']] = 0
         organism.position['x'] += 1
@@ -29,7 +30,7 @@ def move_left(world: np.ndarray, world_state: np.ndarray, organism):
     :param world_state: internal world state
     :return: None
     """
-    if organism.position['x'] != 0:
+    if organism.position['x'] != 0 and world_state[organism.position['y']][organism.position['x'] - 1] == 0:
         world[organism.position['y'], organism.position['x']] = WORLD_BACKGROUND_COLOR
         world_state[organism.position['y'], organism.position['x']] = 0
         organism.position['x'] -= 1
@@ -44,7 +45,8 @@ def move_up(world: np.ndarray, world_state: np.ndarray, organism):
     :param world_state: internal world state
     :return: None
     """
-    if organism.position['y'] != world.shape[0] - 1:
+    if organism.position['y'] != world.shape[0] - 1 and \
+            world_state[organism.position['y'] + 1][organism.position['x']] == 0:
         world[organism.position['y'], organism.position['x']] = WORLD_BACKGROUND_COLOR
         world_state[organism.position['y'], organism.position['x']] = 0
         organism.position['y'] += 1
@@ -59,7 +61,7 @@ def move_down(world: np.ndarray, world_state: np.ndarray, organism):
     :param world_state: internal world state
     :return: None
     """
-    if organism.position['y'] != 0:
+    if organism.position['y'] != 0 and world_state[organism.position['y'] - 1][organism.position['x']] == 0:
         world[organism.position['y'], organism.position['x']] = WORLD_BACKGROUND_COLOR
         world_state[organism.position['y'], organism.position['x']] = 0
         organism.position['y'] -= 1
